39355bf318b814a2275afe383237ef7c7d80f536
Munn@raja2 <ranaw4376@gmail.com>
2024-10-30 21:45:18 -0700

    hello

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-// const express = require("express");
-// const app = express();
-// const socketio = require("socket.io");
-// const http = require("http");
-// const server = http.createServer(app);
-// const io = socketio(server);
-// const path = require("path");
-// const PORT = 3000;
-
-// app.set("view engine", "ejs");
-// app.use(express.static(path.join(__dirname, "public")));
-
-// io.on("connection", function (socket) {
-//   socket.on("send-location", function(data) {
-//     io.emit("receive-location", {id: socket.id, ...data})
-//   })
-
-//   socket.on("disconnect", function(){
-//     io.emit("user-disconnected", socket.id)
-//   })
-// });
-
-
-
-// app.get("/", function (req, res) {
-//   res.render("index");
-// });
-
-// server.listen(PORT);
-
-
-const express = require("express")
+const express = require("express");
 const app = express();
+const socketio = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = socketio(server);
+const path = require("path");
+const PORT = 3000;
 
-app.get("/", function(req, res) {
-    res.send("hello")
-})
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000)
+io.on("connection", function (socket) {
+  socket.on("send-location", function(data) {
+    io.emit("receive-location", {id: socket.id, ...data})
+  })
+
+  socket.on("disconnect", function(){
+    io.emit("user-disconnected", socket.id)
+  })
+});
+
+
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(PORT);
+
+
+// const express = require("express")
+// const app = express();
+
+// app.get("/", function(req, res) {
+//     res.send("hello")
+// })
+
+// app.listen(3000)
